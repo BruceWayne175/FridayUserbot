edf214274f827358c227ba9a7132867d6675013d
Bruce Wayne <75173826+BruceWayne175@users.noreply.github.com>
2020-12-25 19:53:47 +0530

    Update song.py

--- fridaybot/modules/song.py
+++ fridaybot/modules/song.py
@@ -16,7 +16,7 @@ async def WooMai(netase):
     await netase.edit("```Getting Your Music```")
     async with bot.conversation(chat) as conv:
         await asyncio.sleep(2)
-        await netase.edit("`Downloading...Please wait`")
+        await netase.edit("`⬇️Downloading...Please wait`")
         try:
             msg = await conv.send_message(link)
             response = await conv.get_response()
@@ -26,7 +26,7 @@ async def WooMai(netase):
         except YouBlockedUserError:
             await netase.reply("```Please unblock @WooMaiBot and try again```")
             return
-        await netase.edit("`Sending Your Music...`")
+        await netase.edit("`⬆️Sending Your Music...`")
         await asyncio.sleep(3)
         await bot.send_file(netase.chat_id, respond)
     await netase.client.delete_messages(conv.chat_id, [msg.id, response.id, respond.id])
@@ -35,7 +35,7 @@ async def WooMai(netase):
 
 CMD_HELP.update(
     {
-        "song": "**Song**\
+        "song": "**🎶Song**\
 \n\n**Syntax : **`.netease <song name>`\
 \n**Usage :** downloads the song from internet and uploades it."
     }
