4207859177efae7aebf9b984ef49ef2e3e833d51
Bruce Wayne <75173826+BruceWayne175@users.noreply.github.com>
2020-12-25 19:33:02 +0530

    Update afk.py

--- fridaybot/modules/afk.py
+++ fridaybot/modules/afk.py
@@ -93,7 +93,7 @@ async def set_not_afk(event):
                 event.chat_id,
                 "Please set `PRIVATE_GROUP_ID` "
                 + "for the proper functioning of afk functionality "
-                + "Please Seek Support in @FridayOT\n\n `{}`".format(str(e)),
+                + "Please Seek Support in @BruceSL\n\n `{}`".format(str(e)),
                 reply_to=event.message.id,
                 silent=True,
             )
